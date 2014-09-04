2dc371e5e0e9e82a8dc20ab0f972946f0ad16491
kernel <kernel@reimplement.mobi>
2014-09-04 15:18:12 +0300

    Flurry updated - 5.3.0. Ru-blocker added.

--- Gitty/DAAppDelegate.m
+++ Gitty/DAAppDelegate.m
@@ -8,8 +8,9 @@
 
 #import "DAAppDelegate.h"
 
-#import "DAFrameCtrl+Internal.h"
 #import "DALoginCtrl.h"
+#import "DAFrameCtrl+Internal.h"
+#import "WMGeoBlockerWindow.h"
 
 #import "DAGitServer+Creation.h"
 
@@ -33,6 +34,8 @@
 	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 		[DAGitManager.manager scanAllDeletableLocalReposAndDelete];
 	});
+	
+	[WMGeoBlockerWindow checkCurrentCountry];
 	
     return YES;
 }
